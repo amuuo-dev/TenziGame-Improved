ff025d110bf956b8a8eec752fb1babf73fa625fa
amuuo-dev <anthonmuuo92@gmail.com>
2024-06-06 07:51:51 +0300

    create rolls state,best rolls getting from local storage and style

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
 import Dice from "../components/Dice";
 import { nanoid } from "nanoid";
 import Footer from "../components/Footer";
+import Scoreboard from "../components/Scoreboard";
 import Confetti from "react-confetti";
 import { useEffect, useState } from "react";
 const App = () => {
   const [dice, setDice] = useState(allNewDice);
   const [tenzies, setTenzies] = useState(false);
+
+  // Create and initialize states to hold rolls stats
+  const [rolls, setRolls] = useState(0);
+  const [bestRolls, setBestRolls] = useState(
+    JSON.parse(localStorage.getItem("bestRolls")) || 0,
+  );
 
   useEffect(() => {
     const allHeld = dice.every((die) => die.isHeld);
@@ -13,7 +20,9 @@ const App = () => {
     const allSameValue = dice.every((die) => die.value === firstValue);
     if (allHeld && allSameValue) {
       setTenzies(true);
+      Record();
     }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [dice]);
   function generateNewDie() {
     return {
@@ -22,6 +31,10 @@ const App = () => {
       id: nanoid(),
     };
   }
+  // Set bestRolls to localStorage every item bestRolls changes
+  useEffect(() => {
+    localStorage.setItem("bestRolls", JSON.stringify(bestRolls));
+  }, [bestRolls]);
 
   function allNewDice() {
     const newDice = [];
@@ -30,6 +43,11 @@ const App = () => {
     }
     return newDice;
   }
+  function Record() {
+    if (!bestRolls || rolls < bestRolls) {
+      setBestRolls(rolls);
+    }
+  }
   function rollDice() {
     if (!tenzies) {
       setDice((oldDice) =>
@@ -37,9 +55,9 @@ const App = () => {
           return die.isHeld ? die : generateNewDie();
         }),
       );
+      updateRolls();
     } else {
-      setTenzies(false);
-      setDice(allNewDice());
+      resetGame();
     }
   }
   function holdDice(id) {
@@ -57,19 +75,38 @@ const App = () => {
       holdDice={() => holdDice(die.id)}
     />
   ));
+  function resetGame() {
+    setTenzies(false);
+    setDice(allNewDice());
+    setRolls(0);
+  }
+  // Increase rolls counter updating previous state
+  function updateRolls() {
+    return setRolls((oldRolls) => oldRolls + 1);
+  }
   return (
     <div className="app-container shadow-shorter">
+      {tenzies && <Confetti />}
       <main>
-        {tenzies && <Confetti />}
         <h1 className="title">Tenzies</h1>
-        <p className="instructions">
-          Roll until all dice are the same. Click each die to freeze it at its
-          current value between rolls.
-        </p>
+        {!tenzies && (
+          <p className="instructions">
+            Roll until all dice are the same.
+            <br /> Click each die to freeze it at its current value between
+            rolls.
+          </p>
+        )}
+        {tenzies && <p className="winner gradient-text"> YOU WON!</p>}
+
+        <div className="stats-container">
+          <p>Rolls: {rolls}</p>
+        </div>
+
         <div className="container">{dieElement}</div>
         <button onClick={rollDice} className="roll-dice">
           {tenzies ? "New Game" : "Roll"}
         </button>
+        <Scoreboard bestRolls={bestRolls} />
       </main>
       <Footer />
     </div>
